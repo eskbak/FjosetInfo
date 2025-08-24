3105b422513c7cca2e2d5d5c75147a63e1f792a8
Eskil Bakken <bakken.eskil@gmail.com>
2025-08-24 17:31:50 +0200

    text try fix

--- frontend/src/cards/AtBCard.tsx
+++ frontend/src/cards/AtBCard.tsx
@@ -61,9 +61,9 @@ export default function AtBCard({ theme, isDay, stopPlaceId, maxRows = 5 }: Prop
         <>
           <table style={{ width: "100%", borderCollapse: "collapse", tableLayout: "fixed" }}>
             <colgroup>
-              <col style={{ width: "50%" }} />
-              <col style={{ width: "20%" }} />
-              <col style={{ width: "25%" }} />
+              <col style={{ width: "40%" }} />
+              <col style={{ width: "30%" }} />
+              <col style={{ width: "30%" }} />
             </colgroup>
             <thead>
               <tr style={{ textAlign: "left", opacity: 0.65, fontSize: "1.8em" }}>
