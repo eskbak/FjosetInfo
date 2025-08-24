34fbb45f94dd6ca314b142f374ce4e435a040dbb
Eskil Bakken <bakken.eskil@gmail.com>
2025-08-24 17:24:14 +0200

    sindre commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -17,7 +17,11 @@ export default function Header({ todayText }: { todayText: string }) {
         <Clock />
       </div>
 
-      {/* Center logo instead of <h1> */}
+      <h1>
+        Sindre
+      </h1>
+
+      {/* Center logo instead of <h1>
       <div style={{ justifySelf: "center", lineHeight: 0 }}>
         <img
           src={kua}
@@ -27,7 +31,7 @@ export default function Header({ todayText }: { todayText: string }) {
             display: "block",  // avoid baseline gap
           }}
         />
-      </div>
+      </div> */}
 
       <h2
         style={{
